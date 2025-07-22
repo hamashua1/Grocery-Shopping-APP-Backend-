afed64fa4f3121861010b8eff8fe4417e15c75b1
hamashua1 <joshuahamashua@gmail.com>
2025-07-22 21:31:37 +0000

    file updated

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 import express from 'express'
 import cors from 'cors'
-app = express()
+const app = express()
 app.use(cors())
-port = 8000
+const port = 8000
 app.use(express.json())
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+app.listen(port, ()=> {
+    console.log(`server is flying on port ${port}`)
+})
