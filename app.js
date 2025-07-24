c701fa4633263f228af33ac700a8174359a67acc
hamashua1 <joshuahamashua@gmail.com>
2025-07-24 20:20:15 +0000

    files updated

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import 'dotenv/config'
 import connectDB from './database/mongodb.js'
 import itemModel from './schema/item.js'
 import loginModel from './schema/login.js'
+import itemRoutes from './routes/item/api.js'
+
 const app = express()
 connectDB()
 app.use(cors())
@@ -12,18 +14,8 @@ app.use(cookieParser())
 const port = process.env.port || 8000
 app.use(express.json())
 
-
-
-
-
-
-
-
-
-
-
-
-
+// Use routes
+app.use(itemRoutes)
 
 
 app.listen(port, ()=> {
---
+++ routes/item/api.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import itemModel from '../../schema/item.js'
+const router = express.Router()
+
+
+
+// router fot GET endpoint
+router.get('/api/items', async (req,res)=>{
+try{
+     const results = await itemModel.find()
+     res.status(200).json(results)
+}catch(err){
+     res.status(404).json({message:"couldn't fetch response"})
+}
+})
+
+
+//route for POST endpoints
+router.post('/api/items', async(req,res)=>{
+    try{
+    const {name , category , completed} =req.body
+    const item = new itemModel ({name, category, completed})
+    await item.save()
+    res.status(201).json({message:"item added succesfully", item})
+    }catch(err){
+        res.status(401).json({message:"items not added to database "})
+    }
+})
+
+
+//route for PATCH endpoints
+
+export default router
+
