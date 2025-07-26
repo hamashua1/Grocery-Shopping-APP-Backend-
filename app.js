fccb37df5609f97f57f3039e23a307c1253848b5
hamashua1 <joshuahamashua@gmail.com>
2025-07-26 05:47:17 +0000

    file updated

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ import cors from 'cors'
 import cookieParser from "cookie-parser"
 import 'dotenv/config'
 import connectDB from './database/mongodb.js'
-import itemModel from './schema/item.js'
-import loginModel from './schema/login.js'
 import itemRoutes from './routes/item/api.js'
 
 const app = express()
