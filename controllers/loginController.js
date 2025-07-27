eba49fc8f59a5c5df21462a016a8f490faf1daf6
hamashua1 <joshuahamashua@gmail.com>
2025-07-27 01:25:47 +0000

    file updated

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,9 +1,15 @@
 import loginModel from '../schema/login.js'
+import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
+
+
+
 
 export const postLogin = async(req,res)=>{
     try{
     const {name , email, password } = req.body
-    const results = new loginModel({name,email,password})
+    const hashedPassword = await bcrypt.hash(password, 10)
+    const results = new loginModel({name,email,password: hashedPassword})
     await results.save()
     res.status(201).json({message: 'info added to database', results})
     }catch(err){
